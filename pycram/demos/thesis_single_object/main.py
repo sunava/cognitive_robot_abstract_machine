c3d1d5f2b7e973583c7c9356c00f752aa3e4bf57
sunava <hassouna@uni-bremen.de>
2026-04-17 13:03:24 +0200

    unitree less collision

--- pycram/demos/thesis_single_object/main.py
+++ pycram/demos/thesis_single_object/main.py
@@ -12,8 +12,8 @@ from demos.thesis_single_object.single_object_cut_demo import run_single_object_
 # actions = "cut,mix,wipe"
 ROBOT = "g1"
 ACTION = "cut"
-ENVIRONMENT = "test-kitchen-chat"
-OBJECT_KIND = "apple"
+ENVIRONMENT = "apartment"
+OBJECT_KIND = "bread"
 SPAWN_POSITION = None
 SPAWN_YAW = None
 SPAWN_SCALE = 1.0
