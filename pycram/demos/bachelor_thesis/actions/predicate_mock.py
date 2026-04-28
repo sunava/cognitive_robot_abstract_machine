ba2dd7933811032e733ae40be017c5c8ea5127cb
Hanna Becker <beckerh@uni-bremen.de>
2026-04-28 23:11:00 +0200

    add mock for predicates using knowledge

---
+++ pycram/demos/bachelor_thesis/actions/predicate_mock.py
@@ -0,0 +1,8 @@
+"""
+put knowledge queries here, like:
+empty(supportingSurface) = true
+def misplaced(object):
+    get objects location
+    if location = place where belongs -> return false
+    else -> return true
+"""
