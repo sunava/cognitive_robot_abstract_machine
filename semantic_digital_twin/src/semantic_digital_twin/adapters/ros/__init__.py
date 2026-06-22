836a197a70283c852fc61b9683a25d8cb54e4b7c
tom <tom_sch@uni-bremen.de>
2026-06-22 10:31:28 +0200

    [KRROOD] Lucas feedback

--- semantic_digital_twin/src/semantic_digital_twin/adapters/ros/__init__.py
+++ semantic_digital_twin/src/semantic_digital_twin/adapters/ros/__init__.py
@@ -1,10 +1,7 @@
 # Load all ros2 specific json serializers when the ros module is used
-try:
-    from semantic_digital_twin.adapters.ros.ros2_to_semdt_converters import *
-    from semantic_digital_twin.adapters.ros.ros_msg_serializer import *
-    from semantic_digital_twin.adapters.ros.semdt_to_ros2_converters import *
-except ImportError:
-    pass  # ROS message packages not available (non-ROS environment)
+from semantic_digital_twin.adapters.ros.ros2_to_semdt_converters import *
+from semantic_digital_twin.adapters.ros.ros_msg_serializer import *
+from semantic_digital_twin.adapters.ros.semdt_to_ros2_converters import *
 
 # uncomment this if you need to see why ros message parsing fails
 # import os
